de4e4ac7307c667fb7677a575d393efca34a026d
theandersonn <theandersonn@gmail.com>
2021-04-25 14:43:57 -0300

    Remove warnings lint

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
 describe('<App />', () => {
   it('renders learn react link', () => {
-    render(<App />);
-    expect(screen.getByRole('link', { name: /learn react/i })).toBeInTheDocument();
-  });
-});
+    render(<App />)
+    expect(
+      screen.getByRole('link', { name: /learn react/i })
+    ).toBeInTheDocument()
+  })
+})
