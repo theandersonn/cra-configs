fdc958a24d038ec2ea6dc9ab26f7b60737c2c7c0
theandersonn <theandersonn@gmail.com>
2022-07-13 14:12:34 -0300

    stotybook: remove

--- storybook/src/utils/tests/helpers.js
+++
@@ -1,7 +0,0 @@
-import { ThemeProvider } from 'styled-components';
-import { render } from '@testing-library/react';
-
-import theme from '../../styles/theme';
-
-export const renderWithTheme = (children) =>
-  render(<ThemeProvider theme={theme}>{children}</ThemeProvider>);
